831f3785f3b195452fdc38bd0c938d1cd1be4545
eyu-eex <eyuelseyoum92@gmail.com>
2022-08-10 12:48:20 +0300

    fix find_favourite_count function

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -79,7 +79,10 @@ class TweetDfExtractor:
 
         return screen_name
     def find_followers_count(self) -> list:
-        followers_count =
+        followers_count = [tweet['user']['followers_count']
+                           for tweet in self.tweets_list]
+
+        return followers_count
 
     def find_friends_count(self) -> list:
         friends_count =
@@ -93,10 +96,11 @@ class TweetDfExtractor:
         return is_sensitive
 
     def find_favourite_count(self) -> list:
-        followers_count = [tweet['user']['followers_count']
+        favourite_count = [tweet['favorite_count']
                            for tweet in self.tweets_list]
 
-        return followers_count
+        return favourite_count
+        
 
     def find_retweet_count(self) -> list:
         retweet_count =
