f5663116e859cb43324317878958d96eac832df8
eyu-eex <eyuelseyoum92@gmail.com>
2022-08-10 13:06:40 +0300

    fix drop_duplicate function

--- clean_tweets_dataframe.py
+++ clean_tweets_dataframe.py
@@ -1,3 +1,4 @@
+import pandas as pd
 class Clean_Tweets:
     """
     The PEP8 Standard AMAZING!!!
@@ -19,10 +20,9 @@ class Clean_Tweets:
     def drop_duplicate(self, df:pd.DataFrame)->pd.DataFrame:
         """
         drop duplicate rows
-        """
-        
-        ---
-        
+        """        
+        df=df.drop_duplicates()
+
         return df
     def convert_to_datetime(self, df:pd.DataFrame)->pd.DataFrame:
         """
